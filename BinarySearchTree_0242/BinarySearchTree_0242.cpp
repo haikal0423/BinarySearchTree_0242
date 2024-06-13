3b8d5ed185b8353771663de28a3eaef69e014ba5
haikal0423 <mhaikal23azka@gmail.com>
2024-06-13 16:16:31 +0700

    void search(string element, Node*& parent, Node*& currentNode),  currentNode = ROOT; , parent = NULL; ,  while ((currentNode != NULL) && (currentNode->info != element)),  parent = currentNode; ,if (element < currentNode->info),  currentNode = currentNode->leftchild; , else, dan currentNode = currentNode->rightchild;

--- BinarySearchTree_0242/BinarySearchTree_0242.cpp
+++ BinarySearchTree_0242/BinarySearchTree_0242.cpp
@@ -29,12 +29,12 @@ public:
 	}
 
 	void insert(string element) { // Insert a node in the binary search tree
-		{
+		
 			Node* newNode = new Node(element, nullptr, nullptr); // Allocate memmory for the new node
 			newNode->info = element;  // Assign to the data field of the new node
 			newNode->leftchild = nullptr;  //Make the left child of the new node point to null
 			newNode->rightchild = nullptr; // Make the right child of the new node point to null
-		}
+		
 
 		Node* parent = nullptr;
 		Node* currentNode = nullptr;
@@ -53,6 +53,21 @@ public:
 			parent->rightchild = newNode;  // Make the right child of the parent point to the new node
 		}
 	}
-};
 
+    void search(string element, Node*& parent, Node*& currentNode) {  // Locate the node which will be the parent of the new node
+        // This function searches the current Node of the specified Node as well as the currentNode of its parent
+        currentNode = ROOT;
+        parent = NULL;
+
+        while ((currentNode != NULL) && (currentNode->info != element)) {  // While the currentNode is not NULL and the value in the data field of the currentNode is not equal to the value in the data field of the new node
+            parent = currentNode;  // Make the parent point to the currentNode
+            if (element < currentNode->info) {  // If the value in the data field of the new node is less than the value in the data field of the currentNode
+                currentNode = currentNode->leftchild;  // Make the currentNode point to the left child of the currentNode
+            }
+            else {  // If the value in the data field of the new node is greater than the value in the data field of the currentNode
+                currentNode = currentNode->rightchild;  // Make the currentNode point to the right child of the currentNode
+            }
+        }
+    }
+};
 		
