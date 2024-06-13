edfc1688f6e8d072d33ecca30a7393a846cdc643
haikal0423 <mhaikal23azka@gmail.com>
2024-06-13 15:31:49 +0700

    Melanjutkan BinaryTree void insert (string element)

--- BinarySearchTree_0242/BinarySearchTree_0242.cpp
+++ BinarySearchTree_0242/BinarySearchTree_0242.cpp
@@ -27,5 +27,13 @@ public:
 	{
 		ROOT = nullptr; //Intializing ROOT to null
 	}
+
+	void insert(string element) // Insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, nullptr, nullptr); // Allocate memmory for the new node
+		newNode->info = element;  // Assign to the data field of the new node
+		newNode->leftchild = nullptr;  //Make the left child of the new node point to null
+		newNode->rightchild = nullptr; // Make the right child of the new node point to null
+	}
 };
 
