39cb947150a154048d8a4a062ffd3532b87a061e
haikal0423 <mhaikal23azka@gmail.com>
2024-06-13 16:21:33 +0700

    Melanjutkan BinaryTree void preorder(Node* ptr)

--- BinarySearchTree_0242/BinarySearchTree_0242.cpp
+++ BinarySearchTree_0242/BinarySearchTree_0242.cpp
@@ -81,5 +81,17 @@ public:
 			inorder(ptr->rightchild);
 		}
 	}
+
+	void preorder(Node* ptr) {
+		if (ROOT == NULL) {
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
+	}
 };
 		
